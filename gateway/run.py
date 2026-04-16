9c716afb1c0716645e2082737b2c2516043b365a
te.qi_UI-MH <te.qi@ui-mh.com>
2026-04-16 09:52:56 +0800

    fix: progress messages now reply in thread instead of creating new one
    - Add reply_to=event_message_id to all adapter.send() calls in send_progress_messages()
    - Fixes Feishu progress messages creating new threads instead of replying in original thread

--- gateway/run.py
+++ gateway/run.py
@@ -9550,15 +9550,15 @@ class GatewayRunner:
                                     adapter.name,
                                 )
                             can_edit = False
-                            await adapter.send(chat_id=source.chat_id, content=msg, metadata=_progress_metadata)
+                            await adapter.send(chat_id=source.chat_id, content=msg, reply_to=event_message_id, metadata=_progress_metadata)
                     else:
                         if can_edit:
                             # First tool: send all accumulated text as new message
                             full_text = "\n".join(progress_lines)
-                            result = await adapter.send(chat_id=source.chat_id, content=full_text, metadata=_progress_metadata)
+                            result = await adapter.send(chat_id=source.chat_id, content=full_text, reply_to=event_message_id, metadata=_progress_metadata)
                         else:
                             # Editing unsupported: send just this line
-                            result = await adapter.send(chat_id=source.chat_id, content=msg, metadata=_progress_metadata)
+                            result = await adapter.send(chat_id=source.chat_id, content=msg, reply_to=event_message_id, metadata=_progress_metadata)
                         if result.success and result.message_id:
                             progress_msg_id = result.message_id
 
